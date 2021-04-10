c502809ee2cd28d8cfd9726e8b91ab64f5177a67
Akhilesh <akhilesh0707@gmail.com>
2021-04-10 13:13:52 +0530

    Implementing the Firebase Music Source

---
+++ app/src/main/java/com/aqube/music/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,39 @@
+package com.aqube.music.exoplayer
+
+import com.aqube.music.exoplayer.State.*
+
+class FirebaseMusicSource {
+
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = STATE_CREATED
+        set(value) {
+            if (value == STATE_INITIALIZED || value == STATE_ERROR) {
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        return if (state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListeners += action
+            false
+        } else {
+            action(state == STATE_INITIALIZED)
+            true
+        }
+    }
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
