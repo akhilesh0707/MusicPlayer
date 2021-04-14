5a2ee51cd5687cda2f96f1e0875389f79d48fb64
Akhilesh <akhilesh0707@gmail.com>
2021-04-14 20:09:42 +0530

    Changed icons and added fonts

--- app/src/main/java/com/aqube/music/exoplayer/MusicService.kt
+++ app/src/main/java/com/aqube/music/exoplayer/MusicService.kt
@@ -13,6 +13,7 @@ import com.aqube.music.exoplayer.callbacks.MusicPlayerEventListener
 import com.aqube.music.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.aqube.music.others.Constants.MEDIA_ROOT_ID
 import com.aqube.music.others.Constants.NETWORK_ERROR
+import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
@@ -79,7 +80,9 @@ class MusicService : MediaBrowserServiceCompat() {
             mediaSession.sessionToken,
             MusicPlayerNotificationListener(this)
         ) {
-            currentSongDuration = exoPlayer.duration
+            if (exoPlayer.duration != C.TIME_UNSET) {
+                currentSongDuration = exoPlayer.duration
+            }
         }
 
         val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
